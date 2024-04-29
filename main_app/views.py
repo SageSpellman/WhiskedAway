714259f648d8e37e788c391ebf36df0683a2f6d3
Sagespellman <sagespellman@outlook.com>
2024-04-29 19:13:22 -0400

    auth: add login / logout functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     return render(request, 'home.html')
