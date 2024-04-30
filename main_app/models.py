9bf826abfff0f61ff05b9b1dcfa195680eb56f51
Sagespellman <sagespellman@outlook.com>
2024-04-30 13:53:12 -0400

    adding the recipes

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.title
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, requests
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Recipe 
 
 @login_required
 def logout_view(request):
@@ -9,3 +11,27 @@ def logout_view(request):
 
 def home(request):
     return render(request, 'home.html')
+
+class RecipeListView(ListView):
+
+    model = Recipe
+    template_name = 'recipe_list.html'
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe_detail.html'
+
+class RecipeCreateView(CreateView):
+    model = Recipe
+    template_name = 'recipe_form.html'
+    fields = ['title', 'ingredients', 'instructions']
+
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    template_name = 'recipe_form.html'
+    fields = ['title', 'ingredients', 'instructions']
+
+class RecipeDeleteView(DeleteView):
+    model = Recipe
+    template_name = 'recipe_confirm_delete.html'
+    success_url = '/recipes/'
