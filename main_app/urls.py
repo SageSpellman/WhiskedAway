c6e09f1d0ca657d79d8477b7ce2d8f32a2e1d3c5
Sagespellman <sagespellman@outlook.com>
2024-05-01 17:55:51 -0400

    fixing favorite function

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     path('accounts/logout/', views.logout_view, name='logout'),
     path('recipes/', views.recipes, name='recipes'),
     path('recipes/<int:recipe_id>/', views.recipes_detail, name='detail'),
-    path('favorites/', views.favorites, name='favorites'),
+    path('favorites/', views.FavoriteList.as_view(), name='favorites_index'),
+    path('favorites/<int:pk>/', views.FavoriteDetail.as_view(), name='favorites_detail'),
     path('favorites/add/<int:recipe_id>/', views.add_favorite, name='add_favorite'),
-    path('favorites/remove/<int:favorite_id>/', views.remove_favorite, name='remove_favorite'),
-    path('favorites/edit/<int:favorite_id>/', views.edit_favorite, name='edit_favorite'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from .models import Recipe, Favorite
@@ -34,24 +35,13 @@ def recipes_detail(request, recipe_id):
     response = response.json()
     return render(request, 'recipes/detail.html', {'recipe':response})
 
-@login_required
 def add_favorite(request, recipe_id):
-    recipe = Recipe.objects.get(pk=recipe_id)
-    Favorite.objects.create(user=request.user, recipe=recipe)
+    favorite = Favorite( recipe=recipe_id)
+    favorite.save()
     return redirect('favorites')
 
-@login_required
-def remove_favorite(request, favorite_id):
-    favorite = Favorite.objects.get(pk=favorite_id)
-    favorite.delete()
-    return redirect('favorites')
+class FavoriteList(ListView):
+    model = Favorite
 
-@login_required
-def edit_favorite(request, favorite_id):
-    favorite = Favorite.objects.get(pk=favorite_id)
-    return redirect('favorites')
-
-@login_required
-def favorites(request):
-    favorites = Favorite.objects.filter(user=request.user)
-    return render(request, 'favorites.html', {'favorites': favorites})
+class FavoriteDetail(DetailView):
+    model = Favorite
